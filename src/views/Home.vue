3551941ee857ffdee709e41a224a0d26c8f27aea
GBstallman <nduwimanaguymich@gmail.com>
2020-12-09 01:52:31 -0800

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-lg-3">
+                <div  class="login-panel bg-white text-left">
+                    <h1 class="display-3 font-weight-bold">Login</h1>
+                    <form action="">
+                        <div class="form-group">
+                            <label form="email" class="input-label">Username</label>
+                            <input type="text" class="form-control" placeholder="username">
+                        </div>
+                         <div class="form-group">
+                            <label form="email" class="input-label">Password</label>
+                            <input type="password" class="form-control" placeholder="password">
+                        </div>
+                         <div class="form-group">
+                            <button-tab>
+                              <button class="btn btn-primary" @click="login">Login</button>
+                            </button-tab>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
