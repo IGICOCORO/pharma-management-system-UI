5d89319e05954141678faa1ea417521ca9acd347
GBstallman <nduwimanaguymich@gmail.com>
2020-12-28 04:22:42 -0800

    created components add_medecine & add_sell & add_supplier

---
+++ src/components/Add_medecine.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Add_medecine',
+  created() {
+
+  },
+  data() {
+    return {
+    }
+  },
+  props: {
+   
+  },
+  methods: {
+   
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
---
+++ src/components/Add_supplier.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Add_supplier',
+  created() {
+
+  },
+  data() {
+    return {
+    }
+  },
+  props: {
+   
+  },
+  methods: {
+   
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
